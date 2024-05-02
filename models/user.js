4ebc3a21f1bb37861e3cf2d919dc60982caf20c8
Aryan019 <aryan19v@gmail.com>
2024-05-02 19:00:18 +0530

    Making changes

--- models/user.js
+++ models/user.js
@@ -4,16 +4,17 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+      },
   email: {
     type: String,
     required: true,
     unique: true
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
+
   password: {
     type: String,
     required: true
